e563c66fe0b36475e59119c62d094e5062cebaf8
anesh <aneesh_kesavan@yahoo.com>
2022-08-03 16:36:44 -0400

    some

--- upload_to_sftp.py
+++ upload_to_sftp.py
@@ -3,7 +3,7 @@ import paramiko
 def upload(filename):
   sftpserver = "192.168.2.50"
   sftpuser = "anesh"
-  sftppass = "chakku@chakku1"
+  sftppass = ""
   ssh = paramiko.SSHClient()
   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
   ssh.connect(sftpserver,username=sftpuser,password=sftppass)
