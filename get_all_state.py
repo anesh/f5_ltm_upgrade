2965c2389bbbf89c027bba9f6add0999d9c5c7db
ctc <ctc@localhost.localdomain>
2022-08-10 13:25:06 -0400

    updated API call based on tmos version

--- get_all_state.py
+++ get_all_state.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 import sys
+import get_tmos_ver
 requests.packages.urllib3.disable_warnings()
 
 def vip(hostname,username,password,outfile):
@@ -15,11 +16,14 @@ def vip(hostname,username,password,outfile):
   #print "Total VIPS:"+str(len(listofvips))
   totalvips = "Total VIPS:"+str(len(listofvips))
   outfile.write(str(totalvips + '\n'))
+  ver = get_tmos_ver.start(username,password,hostname)
   for vip in listofvips:
     r = requests.get("https://"+hostname+"/mgmt/tm/ltm/virtual/"+vip+"/stats",auth=(username,password),verify=False)
     data =  r.json()
-    #print vip+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/virtual/~Common~'+vip+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
-    vipr= vip+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/virtual/'+vip+'/~Common~'+vip+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
+    if "13" in ver:
+      vipr= vip+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/virtual/'+vip+'/~Common~'+vip+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
+    else:
+      vipr= vip+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/virtual/~Common~'+vip+'/stats']['nestedStats']['entries']['status.availabilityState']['description'
     outfile.write(str(vipr + '\n'))
 
 def pool(hostname,username,password,outfile):
@@ -34,10 +38,14 @@ def pool(hostname,username,password,outfile):
   #print "Total POOLS:"+str(len(listofpools))
   totalpools = "Total POOLS:"+str(len(listofpools))
   outfile.write(str(totalpools + '\n'))
+  ver = get_tmos_ver.start(username,password,hostname)
   for pool in listofpools:
     r = requests.get("https://"+hostname+"/mgmt/tm/ltm/pool/"+pool+"/stats",auth=(username,password),verify=False)
     data =  r.json()
-    poolname = pool+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/pool/'+pool+'/~Common~'+pool+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
+    if "13" in ver:
+      poolname = pool+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/pool/'+pool+'/~Common~'+pool+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
+    else:
+      poolname = pool+"=>"+data['entries']['https://localhost/mgmt/tm/ltm/pool/~Common~'+pool+'/stats']['nestedStats']['entries']['status.availabilityState']['description']
     outfile.write(str(poolname + '\n'))
     mem_response = requests.get("https://"+hostname+"/mgmt/tm/ltm/pool/"+pool+"/members",auth=(username,password),verify=False)
     mem_data = mem_response.json()
